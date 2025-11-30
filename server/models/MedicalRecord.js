e74ec892fbad8f899065bba934e0e04fd447e9f9
Petros Asegid <pasegid@gmail.com>
2025-11-30 21:18:07 +0300

    feat(backend): Add MedicalRecord model

---
+++ server/models/MedicalRecord.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const medicalRecordSchema = new mongoose.Schema({
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  diagnosis: {
+    type: String,
+    required: true
+  },
+  prescription: {
+    type: String,
+    required: true
+  },
+  labResults: {
+    type: String // URL or Path to file
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('MedicalRecord', medicalRecordSchema);
