95c0874cf1a0dc993db8364c3326a2bf0b8c5b9e
Petros Asegid <pasegid@gmail.com>
2025-12-09 23:59:48 +0300

    Phase 3

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 console.log('Auth Routes File Loaded');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, updateUserProfile } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.put('/profile', protect, updateUserProfile);
 router.get('/test', (req, res) => res.send('Auth Route Working'));
 
 module.exports = router;
