3b3079746cbfa68552eebdb6723a953d0834cd33
Petros Asegid <pasegid@gmail.com>
2025-12-09 23:59:41 +0300

    Phase 3

--- server/routes/ambulanceRoutes.js
+++ server/routes/ambulanceRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { requestAmbulance } = require('../controllers/ambulanceController');
+const { requestAmbulance, getMyActiveRequest } = require('../controllers/ambulanceController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/request', protect, requestAmbulance);
+router.get('/my-request', protect, getMyActiveRequest);
 
 module.exports = router;
